3a9ca5af58f199ba294c07680345e3038027633b
Atul <atul.11192@gmail.com>
2019-04-02 19:28:21 +0530

    adding ad model and services

---
+++ src/app/models/Ads.model.ts
@@ -0,0 +1,12 @@
+export class Ads{
+  Userid:number;
+  id:number;
+  Date:string;
+  Title:string;
+  Content:string;
+  Picture:string;
+  Category:string;
+  Subcategory:string;
+  Username:string;
+  Status:string;
+}
